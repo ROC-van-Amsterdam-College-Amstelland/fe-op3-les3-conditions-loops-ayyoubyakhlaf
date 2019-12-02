9aedda9ace3f27da13fcf61c1d750c0b13bca56c
ayyoubyakhlaf <ayyoubyakhlaf@hotmail.com>
2019-12-02 14:29:35 +0100

    4

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,10 @@
-//javascript code
+//javascript codevar saldo = document.getElementById("saldo").value;
+
+var saldo = document.getElementById("saldo").value;
+
+    if (saldo==500) {
+        document.getElementById("tekst").innerHTML = "salaris is gestort"
+    } else {
+        document.getElementById("tekst").innerHTML = "Sorry, je hebt te weinig saldo!";
+    }
+}
--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,5 @@
 //javascript code
+
+var mijngetal = 10;
+
+document.getElementById("tekst").innerHTML = "de waarde van mijn getal is " + mijngetal;
